a93ea0b422209744481c0d59413d5471693a24a1
Junseop <gaon0403@gmail.com>
2019-11-27 17:19:12 +0900

    Delete useless variables, unnecessary lamda

--- LDA.py
+++ LDA.py
@@ -1,4 +1,4 @@
-#require running preprocess.py
+# require running preprocess.py
 import pandas as pd
 from gensim.corpora import Dictionary
 from gensim.models import LdaModel
@@ -7,23 +7,23 @@ import itertools
 
 def makeLDAmodel():
     df = pd.read_pickle('ner_result.pkl')
-    #make df with ner count
+    # make df with ner count
     tokenized_with_ner_count = []
     for ner_dict, tokens in zip(df['ner'], df['tokenized_body']):
-        ner_values = ner_dict.values()
-        ner_list = ner_dict['single_word'] + list(itertools.chain.from_iterable(ner_dict['multi_word']))
+        ner_list = ner_dict['single_word'] + \
+            list(itertools.chain.from_iterable(ner_dict['multi_word']))
         for word in ner_list:
             tokens.remove(word)
         tokens += ner_dict['single_word'] * 10
-        multi_words = list(map(lambda x: ' '.join(x), ner_dict['multi_word']))
+        multi_words = [' '.join(x) for x in ner_dict['multi_word']]
         tokens += multi_words * 10
         tokenized_with_ner_count.append(tokens)
     df['tokenized_with_ner_count'] = tokenized_with_ner_count
     dictionary = Dictionary(df['tokenized_with_ner_count'])
-    df['body_vector'] = df['tokenized_with_ner_count'].apply(dictionary.doc2bow)
+    df['body_vector'] = df['tokenized_with_ner_count'].apply(
+        dictionary.doc2bow)
     corpus = [vector for vector in df['body_vector']]
 
-    temp = dictionary[0]
     id2word = dictionary.id2token
     model = LdaModel(
         corpus=corpus,
@@ -43,4 +43,6 @@ def makeLDAmodel():
         f.write('\n\n'.join(data))
     f.close()
     return model
+
+
 makeLDAmodel()
