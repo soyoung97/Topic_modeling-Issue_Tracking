41000b603d72f3ec6159a5ac9afe9e048bb46069
soyoung97 <lovelife@kaist.ac.kr>
2019-12-02 09:55:39 +0000

    Split LDA to each year

--- LDA.py
+++ LDA.py
@@ -6,13 +6,15 @@ from gensim.models import LdaModel
 from gensim.corpora import Dictionary
 from nltk.corpus import stopwords
 
-NUM_TOPICS = 20
+NUM_TOPICS = 10
 NUM_WORDS = 15
 NER_WEIGHT = 10
 stopwords = stopwords.words('english')
 
-def make_LDA_model(ner_weight):
+def make_LDA_model(ner_weight, year):
     df = pd.read_pickle('ner_result.pkl')
+    df = df[df[' time'] > str(year)]
+    df = df[df[' time'] < str(year+1)]
     # make df with ner count
     tokenized_with_ner_count = []
     for ner_dict, tokens in zip(df['ner'], df['tokenized_body']):
@@ -45,22 +47,23 @@ def make_LDA_model(ner_weight):
     return model
 
 
-def write_topics(model):
+def write_topics(model, year):
     topics = model.show_topics(num_topics=NUM_TOPICS, num_words=NUM_WORDS)
     data = []
     for t in topics:
         data.append(str(t[0]) + '\n' + t[1])
 
     data = list(map(lambda x: str(x.encode('utf-8')), data))
-    with open('LDA_last.log', 'w') as f:
+    with open('./logs/LdaTopics'+str(year)+'.log', 'w') as f:
         f.write('\n\n'.join(data))
     f.close()
+    print("LDA model of year" + str(year) + "Topic log has been saved.")
+    print(topics)
 
 
 if __name__ == '__main__':
-    model = make_LDA_model(NER_WEIGHT)
-
-    model.save('./saves/model.gensim')
-    print('LDA model has just saved as ./saves/model.gensim')
-
-    write_topics(model)
+    for year in [2015, 2016, 2017]:
+        model = make_LDA_model(NER_WEIGHT, year)
+        model.save('./saves/ldamodel-' + str(year) + '.gensim')
+        print('LDA model has just saved as ./saves/ldamodel-' + str(year) + '.gensim')
+        write_topics(model, year)
