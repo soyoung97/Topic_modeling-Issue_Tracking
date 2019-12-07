81158c2532d102985abdea14e3127b36298967e3
Jihee Park <j31d0@users.noreply.github.com>
2019-12-07 21:43:00 +0900

    add representative document selecting

--- off_issue.py
+++ off_issue.py
@@ -7,14 +7,15 @@ from glob import glob
 from gensim.models import LdaModel
 from gensim.corpora import Dictionary
 from sklearn.cluster import DBSCAN
-from scipy import sparse
+from scipy import spatial, sparse
+import math
 import numpy as np
 from Giveme5W1H.extractor.document import Document
 from Giveme5W1H.extractor.extractor import MasterExtractor
 
 def get_articles(data_path, load=False):
     if load:
-        return pandas.read_pickle('ner_result.pkl')
+        return pandas.read_pickle('neuroner_result.pkl')
     total_df = None
     for fname in glob(os.path.join(data_path, '*.json')):
         with open(fname, 'r') as f:
@@ -112,6 +113,25 @@ def do_clustering(model, topic0):
     clustering = DBSCAN(eps=EPS, min_samples=MSAMPLE).fit_predict(tc)
     return clustering
 
+def get_rep_doc(model, docs):
+    dictionary = model.id2word
+    clen = max(dictionary.keys())
+    tc = bow2vec(dictionary, docs['tokenized_body'], clen)
+    tlen = docs.shape[0]
+    for i in range(tlen):
+        tc[i, clen] = docs['timestamp'].iloc[i] * WTime
+    mi = 0
+    mv = None
+    #for i in range(tlen):
+    #    p = 0
+    #    for j in range(tlen):
+    #        cd = tc[i, :] - tc[j, :]
+    #        p = p + math.sqrt((cd.transpose() * cd)[0,0])
+    #    if mv is None or p < mv:
+    #        mi = i
+    #        mv = p
+    return docs.iloc[mi]
+
 def main():
     df = topic_numbering(True)
     model = get_LDA_model('./saves')
@@ -120,7 +140,7 @@ def main():
     topic0['event'] = clustering
     print("clusters: %d " % (max(clustering) + 1))
     for i in range(max(clustering) + 1):
-        rep_doc = topic0[topic0.event == i].iloc[0]
+        rep_doc = get_rep_doc(model, topic0[topic0.event == i])
         print(topic0[topic0.event == i])
         (top_who_answer, top_what_answer, top_when_answer, top_where_answer, top_why_answer, top_how_answer) = get_answer_from_doc(rep_doc)
         print('====================')
