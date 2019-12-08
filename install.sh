cebe8ce79c37686e366393aca9f6270bea48704a
Jihee Park <j31d0@users.noreply.github.com>
2019-12-08 20:15:32 +0900

    change install script to include reuteres

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ NC='\033[0m'
 echo -e "${GREEN}[Info] Start installation${NC}"
 pip install -r requirements.txt
 echo -e "${GREEN}[Info] Install complete, now download nltk dataset${NC}"
-python -c "import nltk; nltk.download('stopwords'); nltk.download('averaged_perceptron_tagger'); nltk.download('maxent_ne_chunker'); nltk.download('words'); nltk.download('punkt')"
+python -c "import nltk; nltk.download('stopwords'); nltk.download('averaged_perceptron_tagger'); nltk.download('maxent_ne_chunker'); nltk.download('words'); nltk.download('punkt'); nltk.download('reuters')"
 echo -e "${GREEN}[Info] Installing corenlp${NC}"
 giveme5w1h-corenlp install
 echo -e "${GREEN}[Info] All done.${NC}"
